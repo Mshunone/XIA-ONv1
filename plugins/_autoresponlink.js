a7d61ad4e13ee538884be1969f33da7911fe7617
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-11-19 19:55:50 +0700

    Update _autoresponlink.js

--- plugins/_autoresponlink.js
+++ plugins/_autoresponlink.js
@@ -5,8 +5,8 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
-• 15 Day / Rp 5k
-• 30 Day / Rp 10k
+• 30 Day / Rp 5k
+• 60 Day / Rp 12k
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
